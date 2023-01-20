2b52d16c80674a41e1b6ceee268eef1cea19df23
Uy575 <usamauy57@gmail.com>
2023-01-20 14:50:52 +0500

    20-1-2023

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useState,useEffect  } from 'react';
 import './App.css';
+import PeraSearch from './components/PeraSearch';
 
 function App() {
+ 
+   const [pera,setPera] = useState([])
+   const [peraLimit,setPeraLimit] = useState(1)
+
+   useEffect(()=>{
+        
+   fetch(`https://hipsum.co/api/?type=hipster-centric&paras=${peraLimit}`)
+   .then(res => res.json())
+   .then(json => setPera(json))       
+   },[peraLimit])
+
+  const settingPeraLimit = e =>{
+    setPeraLimit(e.target.value)
+  } 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sample Peragraph Generator</h1>
+     <PeraSearch peraLimit={settingPeraLimit} />
+     <h1 style={{fontSize:'1rem',fontWeight:'lighter',width:'60rem',margin:'2px auto'}}>{pera}</h1>
     </div>
   );
 }
---
+++ src/components/PeraSearch.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function PeraSearch({peraLimit}) {
+  return (
+
+    <input type='number' placeholder='Enter peragraph' onChange={peraLimit}/>
+  )
+}
+
+export default PeraSearch;
